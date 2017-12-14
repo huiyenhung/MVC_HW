370a52f124e04a274198f398b555bbcd599d0eb5
rts6654 <rts6654@gmail.com>
2017-12-14 21:02:00 +0800

    initial commit

---
+++ MVC_hw/ViewModels/CustomerVW.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_hw.ViewModels
+{
+    public class CustomerVW
+    {
+        public int Id { get; set; }
+        public string 客戶名稱 { get; set; }
+        public int 聯絡人數量 { get; set; }
+        public int 銀行帳戶數量 { get; set; }
+    }
+}
